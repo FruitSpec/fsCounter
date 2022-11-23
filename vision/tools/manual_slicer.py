3a427dde1126eeb2ce48abb17d2a9851ca69fe89
DanaFrodo <dana@fruitspec.com>
2022-11-23 20:49:52 +0200

    manual slicer update logic

--- vision/tools/manual_slicer.py
+++ vision/tools/manual_slicer.py
@@ -40,6 +40,7 @@ def mouse_callback(event, x, y, flags, params):
         frame = print_text(frame, params)
         cv2.imshow(params['headline'], frame)
 
+
 def print_lines(params):
     frame = params['frame'].copy()
     y = int(params['height'] // params['resize_factor'])
@@ -52,6 +53,7 @@ def print_lines(params):
         frame = cv2.line(frame, (x, 0), (x, y), (255, 0, 255), 2)
 
     return frame
+
 
 def print_text(frame, params, text=None):
     if text is None:
@@ -140,7 +142,7 @@ def manual_slicer(filepath, output_path, data=None, rotate=False, index=0, draw_
             frame, params = preprocess_frame(frame, params)
 
             params = init_data_index(params)
-            #params = get_updated_location_in_index(frame, params)
+            # params = get_updated_location_in_index(frame, params)
 
             frame = print_lines(params)
             frame = print_text(frame, params)
@@ -165,6 +167,7 @@ def manual_slicer(filepath, output_path, data=None, rotate=False, index=0, draw_
     # Closes all the frames
     cv2.destroyAllWindows()
 
+
 def preprocess_frame(frame, params):
     height = int(params['height'] // params['resize_factor'])
 
@@ -176,8 +179,8 @@ def preprocess_frame(frame, params):
 
     return frame, params
 
-def get_updated_location_in_index(frame, params):
 
+def get_updated_location_in_index(frame, params):
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
     kp_des = get_fine_keypoints(gray)
 
@@ -197,10 +200,10 @@ def get_updated_location_in_index(frame, params):
                 end = min(max(end + (int(translation[0]) // 2), 0), int(params['height'] // params['resize_factor']))
                 params['data'][params['index']]['end'] = end
 
-
     params['last_kp_des'] = kp_des
 
     return params
+
 
 def init_data_index(params):
     data_indexes = list(params['data'].keys())
@@ -211,7 +214,6 @@ def init_data_index(params):
 
 
 def write_json(params):
-
     temp_str = params['filepath']
     temp_str = temp_str.split('.')[0]
     clip_name = temp_str.split('/')[-1]
@@ -219,6 +221,7 @@ def write_json(params):
     output_file_name = os.path.join(params['output_path'], f'{clip_name}_slice_data.json')
     with open(output_file_name, 'w') as f:
         json.dump(params['data'], f)
+
 
 def load_json(filepath, output_path):
     temp_str = filepath
@@ -270,11 +273,11 @@ def slice_to_trees(data_file, file_path, output_path, resize_factor=3, h=2048, w
         ret, frame = cap.read()
         if ret == True:
             pbar.update(1)
-            #if f_id in hash_ids:
-                #for frame_data in hash[f_id]:
-                #    if not os.path.exists(os.path.join(output_path, f"T{frame_data['tree_id']}")):
-                #        os.mkdir(os.path.join(output_path, f"T{frame_data['tree_id']}"))
-               #     cv2.imwrite(os.path.join(output_path, f"T{frame_data['tree_id']}", f"frame_{f_id}.jpg"), frame)
+            if f_id in hash_ids:
+                for frame_data in hash[f_id]:
+                    if not os.path.exists(os.path.join(output_path, f"T{frame_data['tree_id']}")):
+                        os.mkdir(os.path.join(output_path, f"T{frame_data['tree_id']}"))
+                    cv2.imwrite(os.path.join(output_path, f"T{frame_data['tree_id']}", f"frame_{f_id}.jpg"), frame)
             f_id += 1
         # Break the loop
         else:
@@ -289,10 +292,6 @@ def slice_to_trees(data_file, file_path, output_path, resize_factor=3, h=2048, w
         df.to_csv(os.path.join(output_path, f"T{tree_id}", f"slices.csv"))
 
 
-
-
-
-
 def parse_data_to_trees(data):
     tree_id = 0
     # started_tree = False
@@ -301,7 +300,7 @@ def parse_data_to_trees(data):
     last_state = 0
     trees_data = {}
     for frame_id, loc in data.items():
-        if frame_id == 950:
+        if frame_id == 830:
             a = 1
 
         trees = list(trees_data.keys())
@@ -320,15 +319,15 @@ def parse_data_to_trees(data):
                 trees_data[tree_id] = [{'frame_id': frame_id, 'tree_id': tree_id, 'start': loc['start'], 'end': -1}]
             elif state == 2:
                 if tree_id == trees[-1]:
-                    trees_data[tree_id] = [{'frame_id': frame_id, 'tree_id': tree_id, 'start': -1, 'end': loc['end']}]
+                    trees_data[tree_id].append({'frame_id': frame_id, 'tree_id': tree_id, 'start': -1, 'end': loc['end']})
                 else:
                     raise ValueError("Got tree closing before tree opening")
             elif state == 3:
                 raise ValueError("Got tree closing before tree opening")
             elif state == 4:
-                trees_data[tree_id].append({'frame_id': frame_id, 'tree_id': tree_id, 'start': loc['start'], 'end': -1})
+                trees_data[tree_id].append({'frame_id': frame_id, 'tree_id': tree_id, 'start': -1, 'end': loc['end']})
                 # start new tree
-                trees_data[tree_id + 1] = [{'frame_id': frame_id, 'tree_id': tree_id + 1, 'start': -1, 'end': loc['end']}]
+                trees_data[tree_id + 1] = [{'frame_id': frame_id, 'tree_id': tree_id + 1, 'start': loc['start'], 'end': -1}]
             elif state == 5:
                 tree_id += 1
                 trees_data[tree_id] = [
@@ -484,7 +483,7 @@ def parse_data_to_trees(data):
                 trees_data[tree_id].append(
                     {'frame_id': frame_id, 'tree_id': tree_id, 'start': -1, 'end': loc['end']})
                 # add to next tree
-                trees_data[tree_id ].append({'frame_id': frame_id, 'tree_id': tree_id, 'start': loc['start'], 'end': -1})
+                trees_data[tree_id].append({'frame_id': frame_id, 'tree_id': tree_id, 'start': loc['start'], 'end': -1})
             elif state == 5:
                 trees_data[tree_id].append({'frame_id': frame_id, 'tree_id': tree_id, 'start': loc['start'], 'end': loc['end']})
             elif state == 6:
@@ -501,7 +500,7 @@ def get_state(loc):
         state = 0
     elif loc['start'] is not None and loc['end'] is not None:
         if np.abs(loc['end'] - loc['start']) < 20:
-            if loc['end'] > loc['start']: # assuming moving right
+            if loc['end'] > loc['start']:  # assuming moving right
                 state = 3  # start-end
             else:
                 state = 4  # end-start
@@ -517,13 +516,10 @@ def get_state(loc):
     return state
 
 
-
 if __name__ == "__main__":
-    fp = '/home/yotam/FruitSpec/Sandbox/slicer_test/Result_FSI_1.mkv' #frame 105, middle
-    output_path = "/home/yotam/FruitSpec/Sandbox/slicer_test/R1"
+    fp = r'C:\Users\frodo\Downloads\sliced_pepper\POST-HARVEST-PEPPER-GOOD\1L\FSI.mkv'  # frame 105, middle
+    output_path = r'C:\Users\frodo\Downloads\sliced_pepper\POST-HARVEST-PEPPER-GOOD\1L'
     #manual_slicer(fp, output_path, rotate=True)
 
-    data_file = "/home/yotam/FruitSpec/Sandbox/slicer_test/Result_FSI_1_slice_data.json"
+    data_file = r"C:\Users\frodo\Downloads\sliced_pepper\POST-HARVEST-PEPPER-GOOD\1L\FSI_slice_data.json"
     slice_to_trees(data_file, fp, output_path)
-
-
