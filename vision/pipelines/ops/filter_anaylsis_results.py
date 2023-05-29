077b03fff7310299ad091a4179c70d7d152577f6
matanSht <shteiman.matan@gmail.com>
2023-05-29 13:55:22 +0300

    wip

--- vision/pipelines/ops/filter_anaylsis_results.py
+++ vision/pipelines/ops/filter_anaylsis_results.py
@@ -3,14 +3,32 @@ import numpy as np
 import pandas as pd
 
 
+def get_validated_tracks(data, columns, count_threshold, depth_threshold):
+    data = filter_results(data, columns, count_threshold, depth_threshold)
+    tracks_col = get_column_number(columns)
+    number_of_validated_tracks = len(np.unique(data[:, tracks_col]))
+
+    return number_of_validated_tracks
+
 def filter_results(data, columns, count_threshold, depth_threshold):
 
+    data = get_data_filtered_by_count(data, columns, count_threshold)
+    data = get_data_filtered_by_depth(data, columns, depth_threshold)
+
+    return data
+def get_data_filtered_by_count(data, columns, count_threshold):
     unique_track_ids, counts = get_track_ids_and_count(data, columns)
-    tracks_to_filter = ids_to_filter_by_threshold(counts, count_threshold)
+    ids_to_keep = np.argwhere(counts >= count_threshold)
+    data = data[ids_to_keep.flatten()]
 
+    return data
 
+def get_data_filtered_by_depth(data, columns, depth_threshold):
+    col_id = get_column_number(columns, 'depth')
+    ids_to_keep = np.argwhere(data[:, col_id] < depth_threshold)
+    data = data[ids_to_keep.flatten()]
 
-
+    return data
 
 def load_csv(csv_path):
     data = pd.read_csv(csv_path)
@@ -40,15 +58,11 @@ def get_track_ids_and_count(data, columns):
     return unique_track_ids, counts
 
 
-def ids_to_filter_by_threshold(values, threshold):
-    to_filter = np.argwhere(values < threshold)
 
-    return to_filter
+if __name__ == "__main__":
 
-
-
-
-
-
-
-
+    fp = "/home/matans/Documents/fruitspec/sandbox/Apples_Golan_heights/OLD00000/230523/row_1/tracks_1.csv"
+    depth_threshold = 3.5
+    count_threshold = 3
+    data, columns = load_csv(fp)
+    t = get_validated_tracks(data, columns, count_threshold, depth_threshold)
