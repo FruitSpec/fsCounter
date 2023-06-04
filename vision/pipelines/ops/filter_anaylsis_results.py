f492550b63aad1d7b46ce75fa7403180b8c4e1c8
matanSht <shteiman.matan@gmail.com>
2023-06-04 15:31:01 +0300

    distance measurement

--- vision/pipelines/ops/filter_anaylsis_results.py
+++ vision/pipelines/ops/filter_anaylsis_results.py
@@ -1,4 +1,5 @@
 import os
+from math import radians, cos, sin, asin, sqrt
 import numpy as np
 import pandas as pd
 
@@ -67,6 +68,27 @@ def unique_to_data_ids(data, columns, unique_track_ids):
             ids_to_keep.append(id_)
 
     return ids_to_keep
+
+
+def distance(lat1, lat2, lon1, lon2):
+    # convert from degrees to radians
+    lon1 = radians(lon1)
+    lon2 = radians(lon2)
+    lat1 = radians(lat1)
+    lat2 = radians(lat2)
+
+    # Haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+
+    c = 2 * asin(sqrt(a))
+
+    # Radius of earth in kilometers. Use 3956 for miles
+    r = 6371
+
+    # calculate the result
+    return (c * r) * 1000  # results in M
 
 
 
