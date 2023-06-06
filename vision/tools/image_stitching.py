2f5700552464a0925fab18b685cbc192ff4c4bcc
matanSht <shteiman.matan@gmail.com>
2023-06-06 21:54:26 +0300

    sensor align edge case bug fix

--- vision/tools/image_stitching.py
+++ vision/tools/image_stitching.py
@@ -298,7 +298,13 @@ def match_descriptors_cuda(des1, des2, stream, threshold=0.7, matcherGPU=None):
     match = []
     matchesMask = [[0, 0] for i in range(len(matches))]
     id = 0
-    for m, n in matches:
+    #for m, n in matches:
+    for i in range(matches.__len__()):
+        if matches[i].__len__() < 2:
+            matches = []
+            matchesMask = [[0, 0] for j in range(len(matches))]
+            break
+        m, n = matches[i]
         if m.distance < threshold * n.distance:
             match.append(m)
             matchesMask[id] = [1, 0]
