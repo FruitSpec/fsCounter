7c64f88b55a7d81f99264a5a603f441f4cccb6c9
lihi <lihig@fruitspec.com>
2023-06-21 22:40:34 +0300

    wip

--- vision/trees_slicer/data_exploration.py
+++ vision/trees_slicer/data_exploration.py
@@ -12,7 +12,7 @@ from vision.depth.SVO_to_depth import convert_svo_to_depth_bgr_dgr
 # download_s3_files(s3_path, output_path =output_path, string_param='all_slices', suffix='.csv', skip_existing=True)
 #####################################################################################################################
 folder_path = r'/home/lihi/FruitSpec/Data/customers/DEWAGD'
-paths_svo = find_subdirs_with_file(folder_path, file_name = '.svo', return_dirs=True, single_file=False)
+paths_svo = find_subdirs_with_file(folder_path, file_name = '.svo', return_dirs=False, single_file=False)
 
 # itterate over all svo files and convert to depth:
 for svo_path in paths_svo:
