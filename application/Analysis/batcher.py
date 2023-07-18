86c87fb37d3c8ddf1c4d235330d07779caa049cc
MaayanShriki <99584728+MaayanShriki@users.noreply.github.com>
2023-07-18 13:36:59 +0300

    remove _data_manager_batch_number from batcher

--- application/Analysis/batcher.py
+++ application/Analysis/batcher.py
@@ -20,7 +20,6 @@ class Batcher:
     _shutdown_event = threading.Event()
     _acquisition_start_event = threading.Event()
     _timestamps_log_dict = {}
-    _data_manager_batch_number = 0
     output_dir = ""
 
     def __init__(self, frames_queue, send_data):
@@ -97,11 +96,10 @@ class Batcher:
                 batch = []
             if jai_frame.frame_number % 200 == 0:
                 with self._send_jaized_timestamps_lock:
-                    self._data_manager_batch_number += 1
                     print("sending data from batcher")
                     self._send_data(
                         ModuleTransferAction.JAIZED_TIMESTAMPS,
-                        (self._data_manager_batch_number, self._timestamps_log_dict),
+                        self._timestamps_log_dict,
                         ModulesEnum.DataManager
                     )
                     self.init_timestamp_log_dict()
@@ -115,13 +113,12 @@ class Batcher:
     def stop_acquisition(self):
         with self._send_jaized_timestamps_lock:
             self._acquisition_start_event.clear()
-            self._data_manager_batch_number += 1
             print("BATCHER STOP ACQUISITION")
             if self._timestamps_log_dict["JAI_frame_number"]:
                 print("BATCHER TIME + STOP")
                 self._send_data(
                     ModuleTransferAction.JAIZED_TIMESTAMPS_AND_STOP,
-                    (self._data_manager_batch_number, self._timestamps_log_dict),
+                    self._timestamps_log_dict,
                     ModulesEnum.DataManager
                 )
             else:
@@ -132,4 +129,3 @@ class Batcher:
                     ModulesEnum.DataManager
                 )
             self.init_timestamp_log_dict()
-            self._data_manager_batch_number = 0
