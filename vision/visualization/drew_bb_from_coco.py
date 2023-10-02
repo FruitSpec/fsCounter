be5fae21afec5b9864dfd231f6cecdbe91fa1b70
lihi <lihig@fruitspec.com>
2023-10-02 10:55:30 +0300

    draw_bbox_from_coco

---
+++ vision/visualization/drew_bb_from_coco.py
@@ -0,0 +1,66 @@
+import json
+import os
+import cv2
+
+
+def display_coco_bboxes(coco_file, img_dir, max_height=1000, line_width=1, output_dir=None, save=True):
+    # Load COCO annotations
+    with open(coco_file, 'r') as f:
+        annotations = json.load(f)
+
+    # Define a list of distinct colors
+    all_colors = [
+        (0, 0, 255), (0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0),
+        (255, 0, 255), (127, 127, 255), (127, 255, 127), (255, 127, 127)
+    ]
+    num_classes = len(annotations['categories'])
+    colors = all_colors * (num_classes // len(all_colors)) + all_colors[:num_classes % len(all_colors)]
+
+    color_map = {cat['id']: colors[i] for i, cat in enumerate(annotations['categories'])}
+
+    # Font scale relative to line width
+    font_scale = line_width / 2.0
+
+    # Go through each image in the dataset
+    for image_info in annotations['images']:
+        image_path = os.path.join(img_dir, image_info['file_name'])
+        image = cv2.imread(image_path)
+
+        # Resize image for display if its height is too large
+        height, width = image.shape[:2]
+        scale = max_height / height
+        if scale < 1:
+            image = cv2.resize(image, (int(scale * width), int(scale * height)))
+
+        # Find the annotations for this image
+        for ann in annotations['annotations']:
+            if ann['image_id'] == image_info['id']:
+                # Extract bbox and class information
+                bbox = [int(val * scale) for val in ann['bbox']]
+                cat_id = ann['category_id']
+                cat_name = [cat['name'] for cat in annotations['categories'] if cat['id'] == cat_id][0]
+
+                # Draw bbox and annotations
+                x, y, w, h = bbox
+                cv2.rectangle(image, (x, y), (x + w, y + h), color_map[cat_id], line_width)
+                label = f"{cat_name[0]} {ann.get('score', '')}"
+                cv2.putText(image, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, font_scale, color_map[cat_id],
+                            line_width)
+
+        if save:
+            output_path = os.path.join(output_dir, image_info['file_name'])
+            cv2.imwrite(output_path, image)
+            print (f'Saved: {output_path}')
+
+        else:
+            cv2.imshow('Annotated Image', image)
+            cv2.waitKey(0)
+
+    cv2.destroyAllWindows()
+
+
+coco_file_path = '/home/lihi/FruitSpec/Data/counter/Tomato_FSI_train_260923/annotations/train_coco.json'
+image_directory = '/home/lihi/FruitSpec/Data/counter/Tomato_FSI_train_260923/train2017'
+output_directory = '/home/lihi/FruitSpec/Data/counter/Tomato_FSI_train_260923/annotated_train_images'
+
+display_coco_bboxes(coco_file_path, image_directory, output_dir=output_directory, save=True)
--- vision/visualization/voxel_viewer.py
+++ vision/visualization/voxel_viewer.py
@@ -109,8 +109,8 @@ def create_name():
 
 
 if __name__ == '__main__':
-    data_path = "/home/fruitspec-lab-3/FruitSpec/Data/customers/DEWAGD/training_yoloX/slicer_data_rgd/all_images"
-    files = {"GT": "/home/fruitspec-lab-3/FruitSpec/Data/customers/DEWAGD/training_yoloX/slicer_data_rgd/annotations/all_annotations.json"} #,
+    data_path = "/home/lihi/FruitSpec/Data/counter/Tomato_FSI_train_260923/train2017"
+    files = {"GT": "/home/lihi/FruitSpec/Data/counter/Tomato_FSI_train_260923/annotations/train_coco.json"} #,
              #"yolox": "/home/fruitspec-lab/FruitSpec/Sandbox/yolox_tiny_hires_1024X1024/instances_res3.json",
              #'yoloV5': "/home/fruitspec-lab/FruitSpec/Data/JAI_FSI_V6_COCO/coco_resV5.json"}
     vizualize_coco_results(files, data_path, img_size=[1536, 2048], labels=['fruit'])
