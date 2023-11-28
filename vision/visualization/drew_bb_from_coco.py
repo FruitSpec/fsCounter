04c81e5f76007daeb010d9ecbd7c9ee9377f9f3c
lab3-fruitspec <lab3@fruitspec.com>
2023-11-28 11:50:36 +0200

    draw_bb_from_coco - enabled drawing for GT without score

--- vision/visualization/drew_bb_from_coco.py
+++ vision/visualization/drew_bb_from_coco.py
@@ -45,7 +45,10 @@ def display_coco_bboxes(coco_file, img_dir, max_height=1000, line_width=1, outpu
                 # Draw bbox and annotations
                 x, y, w, h = bbox
                 cv2.rectangle(image, (x, y), (x + w, y + h), color_map[cat_id], line_width)
-                label = f"{cat_name[0]} {round(ann.get('score', ''),2)}"
+                if "score" in ann:
+                    label = f"{cat_name[0]} {round(ann.get('score', ''),2)}"
+                else:
+                    label = f"{cat_name[0]} id{cat_id}"
                 cv2.putText(image, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, font_scale, color_map[cat_id],
                             line_width)
 
@@ -63,11 +66,11 @@ def display_coco_bboxes(coco_file, img_dir, max_height=1000, line_width=1, outpu
 
 if __name__ == "__main__":
 
-    coco_file_path = '/home/fruitspec-lab-3/FruitSpec/Data/Counter/CLAHE_FSI/auto_tagging_old_fsi/citrus_20_11_23/Tagging_Pipeline_Outputs/annotations/coco_val.json'
-    image_directory = '/home/fruitspec-lab-3/FruitSpec/Data/Counter/CLAHE_FSI/auto_tagging_old_fsi/citrus_20_11_23/Tagging_Pipeline_Outputs/val2017'
-    #image_directory = os.path.join(os.path.dirname(coco_file_path), 'all_images')
+    coco_file_path = '/home/fruitspec-lab-3/FruitSpec/Data/Counter/Tomato/FSI/Tomato_FSI_train_261123/new_data_only/all_jsons/15.11_14.39__batch11.json'
+    image_directory =r'/home/fruitspec-lab-3/FruitSpec/Data/Counter/Tomato/FSI/Tomato_FSI_train_261123/new_data_only/all_images'
+    # image_directory = os.path.join(os.path.dirname(coco_file_path), 'all_images')
     # output_directory = os.path.join( os.path.dirname(coco_file_path) ,'frames_annotations')
-    output_directory = '/home/fruitspec-lab-3/FruitSpec/Data/Counter/CLAHE_FSI/auto_tagging_old_fsi/citrus_20_11_23/Tagging_Pipeline_Outputs/tagged_val_images'
+    output_directory = r'/home/fruitspec-lab-3/FruitSpec/Data/Counter/Tomato/FSI/Tomato_FSI_train_261123/new_data_only/debbug'
 
     display_coco_bboxes(coco_file_path, image_directory, output_dir=output_directory, save=True)
 
